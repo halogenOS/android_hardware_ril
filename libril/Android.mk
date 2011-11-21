cfac46d00fd8c4c9a5fcc041ba52b45c879e1ae6
Tom Giordano <tomgiordano83@gmail.com>
2011-11-21 14:48:34 +1100

    libril: allow board to provide libril
    Change-Id: Ie9a78a8890f4ece4f026f3a00ec39ec8e03672fa

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,5 +1,7 @@
 # Copyright 2006 The Android Open Source Project
 
+ifneq ($(BOARD_PROVIDES_LIBRIL),true)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -65,3 +67,4 @@ LOCAL_MODULE:= libril_static
 
 include $(BUILD_STATIC_LIBRARY)
 endif # ANDROID_BIONIC_TRANSITION
+endif # BOARD_PROVIDES_LIBRIL
